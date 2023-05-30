c8e7f1425561ea24284f0e943aa4aa20f97e04f3
Sendeky <spruslasha@gmail.com>
2023-05-30 13:53:07 -0700

    Revert "Revert "updated content written to data.json""
    This reverts commit 13e93f74988b5316d2bb2e87300ed25767b46bd2.
    
    I think I broke something

--- TextbookScraperProject.py
+++ TextbookScraperProject.py
@@ -9,7 +9,6 @@ import time
 
 
 # Textbook URL
-# url = "https://epub-factory-cdn.mheducation.com/publish/sn_f90ad/25/1080mp4/OPS/s9ml/chapter33/reader13.xhtml"
 url = "https://epub-factory-cdn.mheducation.com/publish/sn_f90ad/25/1080mp4/OPS/s9ml/chapter34/reader01.xhtml"
 acessDeniedCount = 0
 retrivedText = []
@@ -154,10 +153,7 @@ def parse():
 
         global retrivedText
         retrivedText.append(r)
-
-        # Write to json file
-        with open('data.json', 'w') as outfile:
-            outfile.write(str(r))
+ 
 
     # error handling
     else:
@@ -199,5 +195,8 @@ if "__main__" == __name__:
     print("yey")
     iterateReaders()
     # parse()
-    print("retrived Text: ", retrivedText)
-    
+    #print("retrived Text: ", retrivedText)
+
+    # Write to json file
+    with open('data.json', 'w') as outfile:
+        outfile.write(str(retrivedText)) 
