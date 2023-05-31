9fbe6591c9ef03c3895cfee8991fdfdb83eceff3
Sendeky <spruslasha@gmail.com>
2023-05-30 20:06:11 -0700

    Turns out MH_TOKEN isn't used and a the permuattion function wasn't doing anything, so I started an almost full rewrite

--- TextbookScraperProject.py
+++ TextbookScraperProject.py
@@ -12,76 +12,81 @@ import time
 url = "https://epub-factory-cdn.mheducation.com/publish/sn_f90ad/25/1080mp4/OPS/s9ml/chapter34/reader01.xhtml"
 acessDeniedCount = 0
 retrivedText = []
+cookies = {}
 
 
 # Cookies
 CloudFront_Signature = ""
 CloudFront_Policy = ""
 CloudFront_Key_Pair_Id = ""
-MH_TOKEN = ""
+MH_TOKEN = ""                   # MH_TOKEN isn't used, but required to have in request so it's filled with a "nil" string
 
 
-# checks if cookie file exists
-if os.path.isfile("cookies.txt"):
-    print("Cookie file found\n")
+# checks if cookie file exists and reads it
+def checkCookies():
+    # checks if cookie file exists
+    if os.path.isfile("cookies.txt"):
+        print("Cookie file found")
 
-    # Open cookies.txt
-    with open("cookies.txt", "r") as f:
-        # reads all lines into a lis
-        lines = f.readlines()
+        # Open cookies.txt
+        with open("cookies.txt", "r") as f:
+            # reads all lines into a lis
+            lines = f.readlines()
 
-    # iterates through lines
-        for line in lines:
+        # iterates through lines
+            for line in lines:
 
-            if "CloudFront_Policy" in line:
-                # CloudFront_Policy
-                CloudFront_Policy = (line.split("CloudFront_Policy=", 1)[1]).strip()            #split finds "CloudFront_Policy=", strip removes newline
-                print("CloudFront_Policy:", CloudFront_Policy, "\n")
+                if "CloudFront_Policy" in line:
+                    # CloudFront_Policy
+                    CloudFront_Policy = (line.split("CloudFront_Policy=", 1)[1]).strip()            #split finds "CloudFront_Policy=", strip removes newline
+                    # print("CloudFront_Policy:", CloudFront_Policy, "\n")
 
-            elif "CloudFront_Signature" in line:
-                # CloudFront_Signature
-                CloudFront_Signature = (line.split("CloudFront_Signature=", 1)[1]).strip()
-                print("CloudFront_Signature:", CloudFront_Signature, "\n")
+                elif "CloudFront_Signature" in line:
+                    # CloudFront_Signature
+                    CloudFront_Signature = (line.split("CloudFront_Signature=", 1)[1]).strip()
+                    # print("CloudFront_Signature:", CloudFront_Signature, "\n")
 
-            elif "CloudFront_Key_Pair_Id" in line:
-                # CloudFront_Key_Pair_Id
-                CloudFront_Key_Pair_Id = (line.split("CloudFront_Key_Pair_Id=", 1)[1]).strip()
-                print("CloudFront_Key_Pair_Id:", CloudFront_Key_Pair_Id, "\n")
+                elif "CloudFront_Key_Pair_Id" in line:
+                    # CloudFront_Key_Pair_Id
+                    CloudFront_Key_Pair_Id = (line.split("CloudFront_Key_Pair_Id=", 1)[1]).strip()
+                    # print("CloudFront_Key_Pair_Id:", CloudFront_Key_Pair_Id, "\n")
 
-            elif "MH_TOKEN" in line:
-                # MH_TOKEN
-                MH_TOKEN = (line.split("MH_TOKEN=", 1)[1]).strip()
-                print("MH_TOKEN:", MH_TOKEN, "\n")
+                elif "MH_TOKEN" in line:
+                    # MH_TOKEN
+                    MH_TOKEN = (line.split("MH_TOKEN=", 1)[1]).strip()
+                    # print("MH_TOKEN:", MH_TOKEN, "\n")
 
-        # Checks if none are empty
-        if CloudFront_Policy == "" or CloudFront_Signature == "" or CloudFront_Key_Pair_Id == "" or MH_TOKEN == "":
-            print("Error: One or more cookies are empty.")
-            exit()
-        else:
-            print("Cookie Reading Success!")
-    
-else: 
-    print("ERROR: cookie file not found\n")
-    print("Check if cookie.txt exists in directory")
-    exit()
+            # Checks if none are empty
+            if CloudFront_Policy == "" or CloudFront_Signature == "" or CloudFront_Key_Pair_Id == "" or MH_TOKEN == "":
+                print("Error: One or more cookies are empty.")
+                exit()
+            else:
+                print("Cookie Reading Success!")
+                
+                # makes cookies global so that you can put cookies.txt info into it
+                global cookies
+                cookies = {
+                'CloudFront-Key-Pair-Id': CloudFront_Key_Pair_Id,
+                'CloudFront-Signature': CloudFront_Signature,
+                'CloudFront-Policy': CloudFront_Policy,
+                'MH_TOKEN': MH_TOKEN,                               # MH_TOKEN isn't used
+                }
+                # print("Cookies: ", cookies)
+
+    else: 
+        print("ERROR: cookie file not found\n")
+        print("Check if cookie.txt exists in directory")
+        exit()
 
 
-# Cookies for the request
-cookies = {
-            'CloudFront-Key-Pair-Id': CloudFront_Key_Pair_Id,
-            'MH_TOKEN': MH_TOKEN,
-            'CloudFront-Signature': CloudFront_Signature,
-            'CloudFront-Policy': CloudFront_Policy,
-            }
-# print("Cookies list: ", cookies, "\n")
-
+### DEPRECATED ###
 # permutation for cookies, if order of cookies has changed
-def permCookies():
-    keys = list(cookies.keys())
-    combinations = list(itertools.permutations(keys))
-
-    for combination in combinations:
-        values = [cookies[key] for key in combination]
+# def permCookies():
+    # keys = list(cookies.keys())
+    # combinations = list(itertools.permutations(keys))
+# 
+    # for combination in combinations:
+        # values = [cookies[key] for key in combination]
         # print("dict(zip): ", dict(zip(combination, values)))
 
 # cycles through reader pages (ie. 01-13)
@@ -115,6 +120,7 @@ def parse():
     # Parses the HTML
     if r.status_code == 200:
         print("Web Request Success!")
+        # print("Web Request Success Cookies: ", cookies)
         html = r.text    # HTML = r.text
         
         soup = BeautifulSoup(html, 'html.parser')   #Parses html with BeautifulSoup
@@ -170,6 +176,9 @@ def parse():
         # Get the value of the Code element
         error_code = root.find('Code').text
 
+        print("ERRORORRR CODDDDEEEEE: ", error_code)
+        print("Cookies after perm: ", cookies)
+
         # Check if the error code is AccessDenied
         if error_code == 'AccessDenied':
             print("Access Denied Error\n")
@@ -179,24 +188,40 @@ def parse():
 
             global acessDeniedCount     # so acessDeniedCount can be accessed
             acessDeniedCount += 1
+            print("Denied count: ", acessDeniedCount)
+            print("Error CODDDDE: ", error_code)
 
             # changes order of cookies to see if that's the issue (sometimes MGH changes the order in which cookies are required)
-            if acessDeniedCount > 16:  # 16 total permuatations of cookies (4 * 4 = 16)
+            if acessDeniedCount > 0:  # 16 total permuatations of cookies (4 * 4 = 16)
                 print("Error, access still denied")
                 print("Check if your cookies have expired. Usually, the KEY-PAIR-ID doesn't change, so check the CloudFront_Signature, CloudFront_Policy, and MH_TOKEN")
                 
             else:
-                permCookies()
+                # permCookies()
                 parse()
 
 
 # runs stuff
-if "__main__" == __name__:
-    print("yey")
-    iterateReaders()
+# if "__main__" == __name__:
+    # print("yey")
+    # iterateReaders()
     # parse()
     #print("retrived Text: ", retrivedText)
 
     # Write to json file
-    with open('data.json', 'w') as outfile:
-        outfile.write(str(retrivedText)) 
+    # with open('data.json', 'w') as outfile:
+        # outfile.write(str(retrivedText)) 
+
+
+# This entire file is a mess and it needs to be completely rewritten.
+# Turns out that MH_TOKEN isn't used
+
+if "__main__" == __name__:
+    print("yey\n")
+
+    checkCookies()
+    
+    # print("Cookies: ", cookies)
+    # TODO
+    # 1. Func to iterate through all the readers (ie. 01-13)
+    # 2. 
